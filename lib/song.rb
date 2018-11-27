3700d97f98916bbbacc865837ea1629e75c5b024
ktochow1 <ktochow1@binghamton.edu>
2018-11-27 21:21:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,18 @@
 class Song 
-  attr_accessor :artist
+
+  attr_accessor :name, :artist
   
   def initialize(name)
     @name = name
+  end
+  
+ def self.new_by_filename(file_name)
+    
+    new_song = file_name.split(' - ')[0] #.gsub! '.mp3', ''
+    new_song = self.new(name)
+    new_song
+    
+    # artist_name = file_name.split(' - ')[0]
   end
   
 end
