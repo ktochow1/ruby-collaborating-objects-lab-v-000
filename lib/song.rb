e5a7c56aeda5fbcad0c768d8a4a6754a7dbc28c1
ktochow1 <ktochow1@binghamton.edu>
2018-11-27 21:30:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,10 +8,10 @@ class Song
   
  def self.new_by_filename(file_name)
     
-    
-    # new_song = self.new(song_name)
     song_name = file_name.split(' - ')[1] #.gsub! '.mp3', ''
-    new_song
+    # new_song = self.new(song_name)
+    
+ 
     
     artist_name = file_name.split(' - ')[0]
     
